337a0b8f5bca16f6322c32a7a5300a9fc57f6167
Maik Schreiber <blizzy@blizzy.de>
2022-02-20 23:25:15 +0100

    update versions of installed software

--- cmd/installer/install.go
+++ cmd/installer/install.go
@@ -10,14 +10,14 @@ const (
 	protocVersion = "3.19.4"
 
 	// https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/
-	protocGenGRPCJavaVersion = "1.44.0"
+	protocGenGRPCJavaVersion = "1.44.1"
 
 	// https://github.com/nvm-sh/nvm/releases
 	nvmVersion = "0.39.1"
 
 	// https://www.azul.com/downloads/zulu-community/?version=java-11-lts&os=ubuntu&architecture=x86-64-bit&package=jdk
-	zuluVersion    = "11.54.23"
-	zuluJDKVersion = "11.0.14"
+	zuluVersion    = "11.54.25"
+	zuluJDKVersion = "11.0.14.1"
 
 	// https://maven.apache.org/download.cgi
 	mavenVersion = "3.8.4"
