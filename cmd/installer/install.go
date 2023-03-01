b4b8bb66fd782c9f11ed8c02afd3217c8d654dd5
Maik Schreiber <blizzy@blizzy.de>
2023-03-01 02:10:36 +0100

    add imagemagick

--- cmd/installer/install.go
+++ cmd/installer/install.go
@@ -24,7 +24,7 @@ var (
 	pacmanPackageNames = []string{
 		"which", "wget", "vim", "nano", "zip", "unzip", "htop", "gcc", "make", "gnu-netcat", "socat", "docker", "fontconfig",
 		"postgresql", "git", "graphviz", "inetutils", "openssh", "man-db", "man-pages", "diffutils", "bash-completion", "fakeroot",
-		"restic", "dnsutils", "ack",
+		"restic", "dnsutils", "ack", "imagemagick",
 
 		// dependencies for Chromium in react-snap
 		"libxcomposite", "libxcursor", "libxdamage", "libxi", "libxtst", "libxss", "libxrandr", "alsa-lib", "atk", "at-spi2-atk", "gtk3", "nss",
