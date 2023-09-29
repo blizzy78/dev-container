fe0a9bbc0086c1f95fb931b119739250f48a193c
Maik Schreiber <blizzy@blizzy.de>
2023-09-29 19:57:34 +0200

    add embedding-server, LanguageTool; update versions of installed software

--- cmd/installer/install.go
+++ cmd/installer/install.go
@@ -4,7 +4,7 @@ package main
 
 const (
 	// https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/
-	protocGenGRPCJavaVersion = "1.57.2"
+	protocGenGRPCJavaVersion = "1.58.0"
 
 	// https://maven.apache.org/download.cgi
 	mavenVersion = "3.9.4"
@@ -23,22 +23,22 @@ var (
 		// https://www.azul.com/downloads/?version=java-17-lts&architecture=x86-64-bit&package=jdk-crac#zulu
 		{
 			jdkMajorVersion: "17",
-			jdkVersion:      "17.0.8",
-			version:         "17.44.17",
+			jdkVersion:      "17.0.8.1",
+			version:         "17.44.55",
 			tag:             "ca-crac",
 		},
 
 		// https://www.azul.com/downloads/?version=java-11-lts&os=linux&architecture=x86-64-bit&package=jdk#zulu
 		{
 			jdkMajorVersion: "11",
-			jdkVersion:      "11.0.20",
-			version:         "11.66.15",
+			jdkVersion:      "11.0.20.1",
+			version:         "11.66.19",
 			tag:             "ca",
 		},
 	}
 
 	// https://golang.org/dl/
-	goVersions = []string{"1.21.0"} // first is default
+	goVersions = []string{"1.21.1"} // first is default
 
 	pacmanPackageNames = []string{
 		"which", "wget", "vim", "nano", "zip", "unzip", "htop", "gcc", "make", "gnu-netcat", "socat", "docker", "docker-buildx", "fontconfig",
