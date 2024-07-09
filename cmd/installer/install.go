4632cdfe9dd31cc188d396eb2c9f73caee1e0210
Maik Schreiber <blizzy@blizzy.de>
2024-07-09 18:35:41 +0200

    update versions of installed software

--- cmd/installer/install.go
+++ cmd/installer/install.go
@@ -4,7 +4,7 @@ package main
 
 const (
 	// https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/
-	protocGenGRPCJavaVersion = "1.64.0"
+	protocGenGRPCJavaVersion = "1.65.0"
 
 	// https://maven.apache.org/download.cgi
 	mavenVersion = "3.9.8"
@@ -54,7 +54,7 @@ var (
 	}
 
 	// https://golang.org/dl/
-	goVersions = []string{"1.22.4"} // first is default
+	goVersions = []string{"1.22.5"} // first is default
 
 	pacmanPackageNames = []string{
 		"which", "wget", "vim", "nano", "zip", "unzip", "htop", "gcc", "make", "gnu-netcat", "socat", "docker", "docker-buildx", "fontconfig",
